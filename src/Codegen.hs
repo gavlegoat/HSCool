54a4ef81cab71310043222e70629720e9a1e4546
Greg <ganders@cs.utexas.edu>
2017-11-10 14:09:45 -0600

    Added to codegen

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -2,6 +2,27 @@ module Codegen where
 
 import Control.Monad
 import Control.Monad.State
+import qualified Data.Map as M
+import Data.Map (Map)
+
+import LLVM.AST
 
 import Types
 import qualified GenerateMaps as G
+
+type SymbolTable = Map String Operand
+
+data BlockState =
+  BlockState { idx :: Int
+             , stack :: [Named Instruction]
+             , term :: Maybe (Named Terminator)
+             } deriving (Show)
+
+data CodegenState =
+  CodegenState { currentBlock :: Name
+               , blocks :: Map Name BlockState
+               , symtab :: SymbolTable
+               , blockCount :: Int
+               , count :: Int
+               , names :: Names
+               } deriving (Show)
